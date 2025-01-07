77b7f0c1c1c391a97c14542f37af8859d8ea8549
Premsirigul <ppremsirigull@gmail.com>
2025-01-07 19:49:53 +0700

    new
    new

---
+++ mylaravel/resources/views/myview.blade.php
@@ -0,0 +1,15 @@
+<h1>My Veiw </h1>
+<?php echo $value_id; ?>
+{{$value_id;}} | {{$myinput}}
+<form action ="{{url('mycontroller')}}"
+    method ="post">
+    @csrf
+    <input type="trxt" name="myinput">
+    <button type="submit">
+        submit
+    </button>
+</form>
+<?php for ($i=0; $i < 10; $i++) { ?>
+    <h1>{{$i}}</h1>
+    
+<?php }?>
