7ef74fbbfb5f385ee50abf9e33ab75210b56c47c
Premsirigul <ppremsirigull@gmail.com>
2025-01-28 17:46:24 +0700

    new4

--- mylaravel/resources/views/layouts/default.blade.php
+++ mylaravel/resources/views/layouts/default.blade.php
@@ -3,10 +3,10 @@
   <!--begin::Head-->
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>AdminLTE 4 | Login Page v2</title>
+    <title>AdminLTE 4 | Login Page</title>
     <!--begin::Primary Meta Tags-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="title" content="AdminLTE 4 | Login Page v2" />
+    <meta name="title" content="AdminLTE 4 | Login Page" />
     <meta name="author" content="ColorlibHQ" />
     <meta
       name="description"
@@ -42,7 +42,7 @@
     />
     <!--end::Third Party Plugin(Bootstrap Icons)-->
     <!--begin::Required Plugin(AdminLTE)-->
-    <link rel="stylesheet" href="{{ url('public/css/adminlte.css') }}" /> 
+    <link rel="stylesheet" href="{{ url('public/css/adminlte.css') }}" /> {{-- แก้ path ตรงนี้ --}}
     <!--end::Required Plugin(AdminLTE)-->
     @yield('styles')
   </head>
@@ -50,6 +50,7 @@
   <!--begin::Body-->
   <body class="bg-body-secondary">
     @yield('content')
+    <!-- /.login-box -->
     <!--begin::Third Party Plugin(OverlayScrollbars)-->
     <script
       src="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.10.1/browser/overlayscrollbars.browser.es6.min.js"
@@ -69,7 +70,7 @@
       crossorigin="anonymous"
     ></script>
     <!--end::Required Plugin(Bootstrap 5)--><!--begin::Required Plugin(AdminLTE)-->
-    <script src="{{ url('/js/adminlte.js') }}"></script>
+    <script src="{{ url('public/js/adminlte.js') }}"></script> {{-- แก้ path ตรงนี้ --}}
     <!--end::Required Plugin(AdminLTE)--><!--begin::OverlayScrollbars Configure-->
     <script>
       const SELECTOR_SIDEBAR_WRAPPER = '.sidebar-wrapper';
