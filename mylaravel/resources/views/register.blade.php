a2ddaa464f3319fe005b40413262272711c04a91
Premsirigul <ppremsirigull@gmail.com>
2025-02-11 16:58:33 +0700

    newlapjavascipt

--- mylaravel/resources/views/register.blade.php
+++ mylaravel/resources/views/register.blade.php
@@ -10,25 +10,43 @@
         <div class="card">
           <div class="card-body register-card-body">
             <p class="register-box-msg">Register a new membership</p>
-            <form action="{{ url('/register') }}" method="post">
+            <form action="{{ url('/register') }}" onsubmit="return myfunction();" method="post">
                 @csrf <!-- ทุกครั้งที่ทำฟอร์มให้ใส่ด้วยไม่งั้นข้อมูลจะไม่ส่งค่าไป -->
               <div class="input-group mb-3">
                 <input type="text" name="name" id ="name" class="form-control" placeholder="Full Name" />
                 <div class="input-group-text"><span class="bi bi-person"></span></div>
+                <div class="valid-feedback">
+                  OK
+                </div>
+                <div class="invalid-feedback" id = "invalid-name">
+                  กรุณาระบุข้อมูล name
+                </div>
               </div>
               <div class="input-group mb-3">
                 <input type="email" name="email" id ="email" class="form-control" placeholder="Email" />
                 <div class="input-group-text"><span class="bi bi-envelope"></span></div>
+                <div class="valid-feedback">
+                   Looks good!
+                </div>
+                <div class="invalid-feedback" id="invalid-email">
+                   Please enter your email.
+                </div>
               </div>
               <div class="input-group mb-3">
                 <input type="password" name="password" id ="pass" class="form-control" placeholder="Password" />
                 <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
+                <div class="valid-feedback">
+                    Looks good!
+                  </div>
+                 <div class="invalid-feedback" id="invalid-pass">
+                   Please enter your password.
+                 </div>
               </div>
               <!--begin::Row-->
               <div class="row">
                 <div class="col-8">
                   <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <input class="form-check-input"id ="mycheckbox" type="checkbox" value="" id="flexCheckDefault" />
                     <label class="form-check-label" for="flexCheckDefault">
                       I agree to the <a href="#">terms</a>
                     </label>
@@ -44,7 +62,7 @@
               </div>
               <!--end::Row-->
             </form>
-            <buttom type ="buttom" class ="btn" onelick= "chickme()">TEST HS</buttom>
+            <button  class ="btn" onelick= "myfunction()">Click me</button>
             
             <!-- /.social-auth-links -->
             <p class="mb-0">
@@ -59,20 +77,61 @@
 
 @section('scripts')
 <script>
-  console.log("hello wold")
-  // เป็นคำสั่งที่ใช้แสดงผลข้อความหรือค่าต่างๆ ลงใน console ของเว็บเบราว์เซอร์หรือใน Node.js
-  //ALERT("Hello world")
+  let $myval
+  var myval2 = "value of myval22"
+  console.log("hello world")
+  //alert("hello")
+function myfunction() {
+  let name = document.getElementById('name') // ดึง input ที่มี id="name"
+  name =$('#name')  // เปลี่ยน name เป็น jQuery Object 
+  let email = document.getElementById('email')
+  email =$('#email') 
+  let pass = document.getElementById('pass')
+  pass =$('#pass') 
+  let mycheckbox = document.getElementById('mycheckbox')
+  //name.value = "My Name Value" ใช้ไม่ได้กับ jQuery
+  //name.val("My Name Value")  // ใช้ .val() ของ jQuery เพื่อกำหนดค่า
+  console.log(name.val() ,email.value ,
+                pass.value ,mycheckbox.checked)
+  if (name.val()=="") {
+      name.addClass('is-invalid')
+      $('#invalid-name').html("<b>กรุณาระบุ name <b>") // เปลี่ยนเนื้อหาภายในของ #myElement
+      return false;
+  }else{
+    name.removeClass('is-invalid')
+  }
+  
+   // ตรวจสอบอีเมลว่ามี @ และ . หรือไม่
+   let emailPattern = /@.*\./;
+            if (!emailPattern.test(email)) {
+                $('#email').addClass('is-invalid');
+                $('#invalid-email').html("<b><u>กรุณากรอกอีเมลให้ถูกต้อง</u></b>");
+                return false;
+            } else {
+                $('#email').removeClass('is-invalid');
+            }
+
+            let passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]+$/;
+            if (!passwordPattern.test(pass)) {
+                $('#pass').addClass('is-invalid');
+                $('#invalid-pass').html("<b><u>รหัสผ่านจะต้องประกอบไปด้วย a-z, A-Z, 0-9</u></b>");
+                return false;
+            } else {
+                $('#pass').removeClass('is-invalid');
+            }
+
+            // เช็คว่า checkbox ถูกติ๊กมั้ย
+            if (!mycheckbox) {
+                $('mycheckbox').addClass('is-invalid');
+                alert('กรุณายอมรับข้อกำหนด');
+                return false;
+            }
+
+            return true;
+}
+//myfunction()
 </script>
 <script>
- // alert("hello world")
-  function clickme(){
-    let name =document.getElementById('name');
-    console.log("hello!",name.value);
-    re
-  }
-  $(document).ready(function () {
-    
-  })
- let myval
+  //console.log(myval2)
 </script>
 @endsection
--- mylaravel/resources/views/user/index.blade.php
+++ mylaravel/resources/views/user/index.blade.php
@@ -2,9 +2,11 @@
 
 @section('content')
     <style>
-        table tbody tr:hover td { /*ใช้กับตารางที่มี tbody เมื่อเอาเม้าส์ไปวางที่แถวข้อมูลทั้งแถวนั้นที่มีเม้าส์วางจะเปลี่ยนสี*/
+        table tbody tr:hover td {
+            /*ใช้กับตารางที่มี tbody เมื่อเอาเม้าส์ไปวางที่แถวข้อมูลทั้งแถวนั้นที่มีเม้าส์วางจะเปลี่ยนสี*/
             color: hotpink;
-            transition: color 0.3s ease-in-out; /* ระยะเวลาการเปลี่ยนสี + ทำให้สีค่อยๆเปลี่ยนแบบสวยๆ */
+            transition: color 0.3s ease-in-out;
+            /* ระยะเวลาการเปลี่ยนสี + ทำให้สีค่อยๆเปลี่ยนแบบสวยๆ */
         }
     </style>
 
@@ -21,7 +23,6 @@
                                 <th style="width: 10px">#</th>
                                 <th>Name</th>
                                 <th>Email</th>
-                                
                                 <th style="width: 240px"></th>
                             </tr>
                         </thead>
@@ -37,11 +38,13 @@
                                                 <i class="fas fa-edit"></i> Edit
                                             </button>
                                         </a>
-                                        <form action="{{ url('/user') }}" method="post" style="display: inline">
+                                        <form action="{{ url('/user') }}" onclick="return false;" method="post"
+                                            id="user-{{ $user->id }}" style="display: inline"> {{-- id="user-{{ $user->id }}" ใช้เพื่อให้เจาะจงไปยังผู้ใช้ได้ถูกคนโดยจะเป็น user-ลำดับของid --}}
                                             @csrf
                                             @method('delete')
                                             <input type="hidden" name="id" value="{{ $user->id }}">
-                                            <button type="submit" class="btn btn-danger btn-sm">
+                                            <button type="submit" class="btn btn-danger btn-sm"
+                                                onclick="confirm_delete('{{ $user->id }}')">
                                                 <i class="fas fa-trash-alt"></i> Delete
                                             </button>
                                         </form>
@@ -52,26 +55,65 @@
                     </table>
                 </div>
             </div>
+            <button class="btn" onclick="confirm_delete('clickMe')">Click Me</button>
+            {{-- /.card --}}
         </div>
     </div>
 @endsection
+
 @section('scripts')
-<script>
-function clickme(){
-    Swal.fire({
-  title: "Are you sure?",
-  text: "You won't be able to revert this!",
-  icon: "warning",
-  showCancelButton: true,
-  confirmButtonColor: "#3085d6",
-  cancelButtonColor: "#d33",
-  confirmButtonText: "Yes, delete it!"
-}).then((result) => {
-  if (result.isConfirmed) {
-    return true;
-  }
-});
-return fales;
-}
-</script>
+    {{-- <script>
+        function confirm_delete() {
+            Swal.fire({
+                title: "Are you sure?",
+                text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, delete it!"
+            }).then((result) => {
+                console.log("Result", result);
+                if (result.isConfirmed) {
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Your file has been deleted.",
+                        icon: "success"
+                    }).then(() => {
+                        document.getElementById("user").submit()
+                    });
+                }
+            });
+        }
+    </script> --}}
+    <script>
+        function confirm_delete(userId) {
+            Swal.fire({
+                title: "Are you sure?",
+                text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, delete it!"
+            }).then((result) => {
+                console.log("Result", result);
+                if (result.isConfirmed) {
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Your file has been deleted.",
+                        icon: "success"
+                    }).then(() => {
+                        if (userId === 'clickMe') {
+                            console.log("Result", result);
+                        } else {
+                            console.log("Result", result);
+                            document.getElementById("user-" + userId)
+                                .submit(); // "user-" + userId ใช้เพื่อให้เจาะจงไปยังผู้ใช้ได้ถูกคนโดยจะเป็น user-ลำดับของid จะได้ลบถูก
+                        }
+                    });
+                }
+            });
+        }
+    </script>
 @endsection
